0289cfc6543f937444620c9012c1b15a97804b66
Tsatyr Ismailyan <palint1@mail.ru>
2023-03-19 23:32:17 +0300

    add commit

---
+++ lesson1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Пример 1
+
+/*
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+int quad = number2 * number2;
+
+if(number1 == quad)
+{
+    Console.WriteLine("yes");
+}
+else 
+{
+    Console.WriteLine("no!!!");
+}
+*/
+
+// Пример 2 
+/*
+Console.Write("Введите позитивное число  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+     Console.Write(current + " "); // прибаваили к значению пробел чтобы красиво выводить 
+     current ++;  // current -= / += / и тд даже += num (любая переменная)
+
+}
+*/
+Убираем числа с конца 
+12345 / 10 = 1234
+12345 / 100 = 123
+12345 / 1000 = 12
+12345 / 10000 = 1
+Убираем числа с начала 
+12345 % 10 = 5
+12345 % 100 = 45
+12345 % 1000 = 345
+12345 % 10000 = 2345
+
+
+
+
